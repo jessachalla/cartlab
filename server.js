da9a15c67f5cccc06328635106dd3d2e9274be6a
Jessa Challa <owner@Owners-MacBook-Pro.local>
2019-06-05 18:02:30 -0400

    added notes

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-const express = require('express');
-
-const app = express();
-app.use(express.json());
-app.get('/api', (req, res) => {
-    res.send('Hello, World');
-})
-
-const cartItems = require('./cartItems');
-app.use('/', cartItems);
+const express = require('express'); //import express
+const app = express(); //new instance of express
+app.use(express.json()); //lets me use req, res example: req.body.example
+// app.get('/api', (req, res) => {
+//     res.send('Hello, World');
+// })
+const cartItems = require('./cartItems'); //use the routing in cartItems.js
+app.use('/', cartItems); //anytime it sees '/' it uses cartItems.js
 
 const port = 3003;
 
-app.listen(port, () => console.log(`listening on port: http://localhost:${port}`));
+app.listen(port, () => console.log(`listening on port: http://localhost:${port}`)); //listening to anything that is coming in on port assigned
